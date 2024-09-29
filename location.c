446b833c9d92b61a05539be64581a5b3e1f27578
Isaque Franklin <isaquefrankli@gmail.com>
2024-09-29 11:48:55 -0300

    Locations update.

--- location.c
+++ location.c
@@ -16,5 +16,26 @@ locs[] = {
 static unsigned locationOfPlayser = 0;
 
 void executeLook(const char *noun){
+    if(noun != NULL && strcmp(noun, "around") == 0){
+        printf("You are in %s.\n", locs[locationOfPlayser].description);
+    } else {
+        printf("I don't understand what you want to see.\n");
+    }
+}
 
+void executeGo(const char *noun){
+    unsigned i;
+    for(i = 0; i < numberOfLocations; i++){
+        if(noun != NULL && strcmp(noun, locs[i].tag) == 0){
+            if(i == locationOfPlayser){
+                printf("You can't get much closer than this.\n");
+            } else {
+                printf("OK.\n");
+                locationOfPlayser = i;
+                executeGo("around");
+            }
+            return;
+        }
+    }
+    printf("I don't understand where you want to go.\n");
 }
